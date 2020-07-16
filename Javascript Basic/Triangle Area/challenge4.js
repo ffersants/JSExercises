e0fd02f1680ce5cdcd45ae822c0f2ca72418ad17
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2020-07-15 21:35:17 -0300

    Feito alterações. Testar no MAC.

--- Javascript Basic/Triangle Area/challenge4.js
+++ Javascript Basic/Triangle Area/challenge4.js
@@ -1,5 +1,10 @@
 const fields = document.querySelectorAll("[required]");
 
+// butaum = document.getElementById("result");
+// butaum.disabled = true - butaum disabled até que os inputs sejam validos... que tal?
+// input1 = document.getElementById("side1");
+// input1.focus()
+
 for ( field of fields ){
     field.addEventListener("invalid", customValidation);
     //"invalid" é tido como um evento que é disparado 
@@ -7,46 +12,66 @@ for ( field of fields ){
     
     //The invalid event fires when a submittable element has been 
     //checked for validity and doesn't satisfy its constraints.
+    field.addEventListener("blur", customValidation);
 }
 
 function customValidation(event){
-    const field = event.target// com isso temos uma referência ao objeto que enviou o evento
-    field.setCustomValidity("uba uba") 
-    //lógica para verificar se existem erros
     
+
+    //impede o bubble de ser disparado 
+    event.preventDefault()
+
+    const field = event.target// com isso temos uma referência ao objeto que enviou o evento
+    //field.setCustomValidity("uba uba") bubble bloqueado 
+    //console.log(field.type)
+    //lógica para verificar se existem erros
 //essa funcao eu nao entendi
     function verifyErrors(){
         let foundError = false;
 
         for ( let error in field.validity){
             
-            if ( error != "customError" && field.validity[error]){
+            if (field.validity[error] && !field.validity.valid){
                 foundError = error;//so vai retornar um outro true referente ao atributo valueMissing
             }
 
             return foundError;
         }
+
+        //console.log(foundError)
     }
 
     const error = verifyErrors()
-    console.log("Erro existe: " + error)
+
+    const smallError = field.parentNode.querySelector("small.error")
 
     if (error){
-        field.setCustomValidity("Este campo eh obrigatorio!")
+        
+        smallError.innerHTML = 'Campo Obrigatório'
     } else {
-        field.setCustomValidity("")
+        smallError.classList.remove("active");
+        smallError.innerHTML = "";
     }
+
+    //console.log("Erro existe: " + error)
+
+    //altera mensagem do buble, que não está sendo masi disparado graças ao preventDefault no início da função
+    // if (error){
+    //     field.setCustomValidity("Este campo eh obrigatorio!")
+    // } else {
+    //     field.setCustomValidity("")
+    // }
 
     
 }
+
+
 
 document.querySelector("form").addEventListener("submit", event =>{
     operation();
     //cancels the event if it is cancelable, 
     //meaning that the default action that belongs to the event will not occur.
     //Clicking on a "Submit" button, prevent it from submitting a form
-    
-    
 })
 
 function operation(){
@@ -63,7 +88,10 @@ function operation(){
 
     var s = (side1 + side2 + side3)/2;
     var area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
-
+    area = area.toString();
+    dotIndex = area.indexOf('.') + 3;
+    area = area.slice(0, dotIndex)
+    console.log(typeof area)
     result = document.getElementById("result");
     result.textContent = area;
     result.className = 'formatar-resultado'
