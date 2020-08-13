35d286bcf506484731a82c98897286645cb9e7d8
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2020-08-12 23:20:23 -0300

    Refatorar amanhã.

--- Javascript Basic/Minefield of numbers/challenge8.js
+++ Javascript Basic/Minefield of numbers/challenge8.js
@@ -1,4 +1,30 @@
 inputArea = document.getElementById("number-inputer");
-inputArea.__proto__.stepDown = false;
-inputArea.__proto__.stepUp = false; 
-console.log(inputArea.__proto__)
+
+document.addEventListener('click', () => {
+    shooter = (event.target);
+    setInput(shooter)
+}
+);
+
+
+function setInput(element){
+    element = (shooter.textContent);
+    element = parseInt(element);
+    
+    element === NaN ? false : inputArea.value = element;
+}
+
+function checkInput(){
+    if (inputArea.value == ''){
+        alert('insira um número')
+    } 
+    else {
+        randomNumber = Math.floor(Math.random() * 10);
+
+        if(randomNumber == inputArea.value){
+            alert("Parabéns! Você acaba de ganhar uma chance de ganhar mil reais!!")
+        } else{
+            alert('aaah não foi dessa vez..')
+        }
+    }//if ends
+}
