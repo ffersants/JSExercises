aa625b3eb0ca929304493273fa5fed2f2ce46253
ffersants <60589945+ffersants@users.noreply.github.com>
2020-08-13 12:47:05 -0300

    Feito. Challenge finalizado

--- Javascript Basic/Minefield of numbers/challenge8.js
+++ Javascript Basic/Minefield of numbers/challenge8.js
@@ -2,13 +2,19 @@ inputArea = document.getElementById("number-inputer");
 
 document.addEventListener('click', () => {
     shooter = (event.target);
-    setInput(shooter)
+    setInput(shooter.textContent);
 }
 );
 
+document.addEventListener("keydown", () => {
+    tecla = event.key;
+    tecla === `Enter` ? checkInput() : false
+    tecla = parseInt(tecla);
+    setInput(tecla);
+})
 
 function setInput(element){
-    element = (shooter.textContent);
+    console.log(element);
     element = parseInt(element);
     
     element === NaN ? false : inputArea.value = element;
@@ -16,15 +22,17 @@ function setInput(element){
 
 function checkInput(){
     if (inputArea.value == ''){
-        alert('insira um número')
+        alert('insira um número');
     } 
     else {
         randomNumber = Math.floor(Math.random() * 10);
 
         if(randomNumber == inputArea.value){
-            alert("Parabéns! Você acaba de ganhar uma chance de ganhar mil reais!!")
+            alert("Parabéns! Você acaba de ganhar uma chance de ganhar mil reais!!");
+            inputArea.value = ``;
         } else{
-            alert('aaah não foi dessa vez..')
+            alert('aaah não foi dessa vez..');
+            inputArea.value = ``;
         }
-    }//if ends
+    }
 }
