040ea69b7290da529e60821ee0e7e0b26c1f4a39
ffersants <60589945+ffersants@users.noreply.github.com>
2020-08-26 14:30:01 -0300

    Chanllenge 10 finalizado.

--- Javascript Basic/Multiplication and division/challenge10.js
+++ Javascript Basic/Multiplication and division/challenge10.js
@@ -1,26 +1,69 @@
-const BUTTONS = document.querySelectorAll("button");
-for(button of BUTTONS){
-    button.addEventListener("click", mainEvent)
-}
-
 firstNumber = document.getElementById("first-number");
 secondNumber = document.getElementById("second-number");
 
-firstNumber.addEventListener("blur", () => {
-    console.log(typeof firstNumber.value)
-    if(firstNumber.value = `1`) {
-        firstNumber.validity.valid
-        console.log("fojr")
+
+const BUTTONS = document.querySelectorAll("button");
+    for(button of BUTTONS){
+        button.addEventListener("click", mainEvent)
     }
-})
+
+var inputs = document.querySelectorAll("[required]");
+    for (input of inputs){ 
+        input.addEventListener("blur", (e) => {
+            field = event;
+            console.log(inputs)
+            if(!field.target.validity.valueMissing){
+                field.srcElement.className += (" acstive")
+            } else {
+                field.srcElement.className = ("form-control")
+            }
+        })
+    }
 
 function mainEvent(event){
     event.preventDefault();
+    
     if(firstNumber.value && secondNumber.value !== ""){
-        
-        event.path[0].outerText === "Multiply" ? multiply() : divide()
-    } 
-   
+        event.path[0].outerText === "Multiply" ? multiply(firstNumber.value, secondNumber.value) : divide(firstNumber.value, secondNumber.value)
+    } else {
+        Swal.fire({
+            icon: `question`,
+            title: `Input invalid`,
+            text: `Well, there's nothing here...`,
+            confirmButtonText: 'Ok'
+        })
+
+        firstNumber.value = ``;
+        secondNumber.value = ``;
+        firstNumber.className = "form-control";
+        secondNumber.className = "form-control";
+    }
 }
 
-//multiply = document.getElementById("multiply");
+function multiply(number1, number2){
+    console.log(number1 * number2)
+    Swal.fire({
+        title: `Multiply`,
+        text: number1 * number2,
+        confirmButtonText: 'Cool'
+    })
+
+    firstNumber.value = ``;
+    secondNumber.value = ``;
+    firstNumber.className = "form-control";
+    secondNumber.className = "form-control";
+}
+
+function divide(number1, number2){
+    console.log(number1 / number2)
+    Swal.fire({
+        title: `Divide`,
+        text: number1 / number2,
+        confirmButtonText: 'Cool'
+    })
+
+    firstNumber.value = ``;
+    secondNumber.value = ``;
+    firstNumber.className = "form-control";
+    secondNumber.className = "form-control";
+}
