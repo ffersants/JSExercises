95064410a253ba315435aff409931251ffe6b452
ffersants <60589945+ffersants@users.noreply.github.com>
2020-09-15 13:44:45 -0300

    Usando string methods como split e substr.

---
+++ Javascript Basic/File name extension/challenge14.js
@@ -0,0 +1,23 @@
+filename = "docWord.docx";
+
+extension = filename.substr(filename.indexOf("."), filename.length);
+
+console.log(extension)
+
+//resolucao do site
+console.log(filename.split(".").pop())
+
+
+// o metodo split separa uma string em um array de string, tomando como referencia o parametro passado, que eh tido como o separador
+
+//Quando encontrado, o separador é removido da string e as substrings são retornadas em um array. 
+frase1 = "frase-dividida-com-base-nos-hifens";
+console.log(frase1.split("-"))
+
+//Se o separador não for encontrado ou for omitido, o array irá conter um elemento consistindo da string inteira
+frase2 = "frase-inteira-e-nao-dividida"
+console.log(frase2.split("."))//separador nao encontrado
+console.log(frase2.split())
+
+//Se o separador é uma string vazia, str será convertido em um array de caracteres.
+console.log(filename.split(""))
---
+++ Javascript Basic/User defined variable/challenge13.js
@@ -0,0 +1,13 @@
+a = document.querySelectorAll(".button")
+
+for( b of a) {
+    b.addEventListener("click", () => {
+        name = document.getElementById("name").value;
+        var var_name = ""
+        var n = name
+        this[var_name] = n;
+        console.log(var_name)
+    })
+}
+
+
